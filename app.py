c1122a379d156787f755358e3b28dc1af835101d
Abigayle Reeves <16areeves@gmail.com>
2022-05-15 17:35:51 -0400

    Updating code

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ mongo = PyMongo(app)
 
 
 # Define the main route to the HTML page
-@app.route("/")
+@app.route("/") 
 def index():
    mars = mongo.db.mars.find_one()
    return render_template("index.html", mars=mars)
