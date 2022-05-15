2b6dfcd8f27896c6b2d95f6c1020359959bf6fc4
Abigayle Reeves <16areeves@gmail.com>
2022-05-15 16:37:54 -0400

    Pushing module code

--- scraping.py
+++ scraping.py
@@ -1,20 +1,19 @@
-# Import Splinter and BeautifulSoup
+# Import Splinter, BeautifulSoup, and Pandas
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import datetime as dt
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def scrape_all():
     # Initiate headless driver for deployment
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=True)
-    
-    # Define fx Python will use to pull data
+
     news_title, news_paragraph = mars_news(browser)
-    
-    # Run all scraping functions and store results in dictionary
+
+    # Run all scraping functions and store results in a dictionary
     data = {
         "news_title": news_title,
         "news_paragraph": news_paragraph,
@@ -22,30 +21,26 @@ def scrape_all():
         "facts": mars_facts(),
         "last_modified": dt.datetime.now()
     }
-    
+
     # Stop webdriver and return data
     browser.quit()
     return data
 
 
+def mars_news(browser):
 
-### Featured Article from Mars News Site
-
-# this was added to define it as a f(x), so it can be used repeatedly w/o error. Use the browser addition, to tell Python to use the variable defined previously - w/o this it won't work
-def mars_news(browser): 
+    # Scrape Mars News
     # Visit the mars nasa news site
-    url = 'https://redplanetscience.com'
+    url = 'https://data-class-mars.s3.amazonaws.com/Mars/index.html'
     browser.visit(url)
 
     # Optional delay for loading the page
     browser.is_element_present_by_css('div.list_text', wait_time=1)
 
-
-    # Parse the HTML
+    # Convert the browser html to a soup object and then quit the browser
     html = browser.html
     news_soup = soup(html, 'html.parser')
-    
-    
+
     # Add try/except for error handling
     try:
         slide_elem = news_soup.select_one('div.list_text')
@@ -57,25 +52,17 @@ def mars_news(browser):
     except AttributeError:
         return None, None
 
-    # Return the title and paragraph (summary) of the article
     return news_title, news_p
-    
 
-
-### Featured Images from JPL Space Images
 
 def featured_image(browser):
-
     # Visit URL
-    url = 'https://spaceimages-mars.com'
+    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
     browser.visit(url)
-
 
     # Find and click the full image button
     full_image_elem = browser.find_by_tag('button')[1]
     full_image_elem.click()
-
-
 
     # Parse the resulting html with soup
     html = browser.html
@@ -90,40 +77,27 @@ def featured_image(browser):
         return None
 
     # Use the base url to create an absolute url
-    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+    img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
 
     return img_url
 
-
-### Featured Table from Mars Facts
-
 def mars_facts():
-    
-     # Add try/except for error handling
+    # Add try/except for error handling
     try:
         # Use 'read_html' to scrape the facts table into a dataframe
-        df = pd.read_html('https://galaxyfacts-mars.com')[0]
+        df = pd.read_html('https://data-class-mars-facts.s3.amazonaws.com/Mars_Facts/index.html')[0]
 
     except BaseException:
         return None
-    
-    
-    # Assign columns and set index of df
+
+    # Assign columns and set index of dataframe
     df.columns=['Description', 'Mars', 'Earth']
     df.set_index('Description', inplace=True)
     
-
-
-    # Convert the dataframe back into html code, add bootstrap
+    # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
 
-
-
-# # Quit the browser, to save memory and to save the computer's life
-# browser.quit() # not needed, added to scrape_all fx
-
-
 if __name__ == "__main__":
+
     # If running as script, print scraped data
     print(scrape_all())
-
